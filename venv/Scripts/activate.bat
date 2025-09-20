ebf439ab26b66b0d3334659f4e8f126ca0c49a98
Karenrioz <riosdaniela.014.z@gmail.com>
2025-09-20 13:25:10 -0500

    Creacion del modulo facturas

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\juand\Desktop\front-ecofact\venv"
+set "VIRTUAL_ENV=C:\Users\Karen\Desktop\front-ecofact\front-ecofact\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
